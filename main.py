c74db4d98ed1a6958b2ed9d322674996ef59e5d2
Jonathan Porter <j.porter712@gmail.com>
2025-05-15 14:24:37 -0700

    init

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import logging
+
+# jamf-pro-sdk imports
+from jamf_pro_sdk import JamfProClient, SessionConfig
+from jamf_pro_sdk.clients.auth import ApiClientCredentialsProvider
+from jamf_pro_sdk.helpers import logger_quick_setup
+from jamf_pro_sdk.clients.pro_api.pagination import Paginator, FilterField, SortField, filter_group
+from jamf_pro_sdk.models.pro.mdm import SetRecoveryLockCommand
+from jamf_pro_sdk.models.pro.mdm import EnableLostModeCommand
+from jamf_pro_sdk.models.pro.computers import ComputerGroupMembership
+
+# Environment constants
+JAMF_CLIENT_ID = os.environ.get("JAMF_CLIENT_ID")
+JAMF_CLIENT_SECRET = os.environ.get("JAMF_CLIENT_SECRET")
+#JAMF_SDK_BASE_URL = os.environ.get("JAMF_SDK_BASE_URL")
+JAMF_BASE_URL = os.environ.get("JAMF_BASE_URL")
+GROUP_ID = 430
+
+# Session config
+config = SessionConfig()
+config.timeout = 30
+config.max_retries = 3
+config.max_concurrency = 5
+config.return_exceptions = True
+config.verify = True
+config
+
+# Logging
+logger_quick_setup(level=logging.DEBUG)
+
+# initialize JamfProClient
+client = JamfProClient(
+    server=JAMF_BASE_URL,
+    credentials=ApiClientCredentialsProvider(JAMF_CLIENT_ID, JAMF_CLIENT_SECRET),
+    session_config=config
+)
+
+# Retrieve Computer Group Data
+computer_group_data = client.classic_api.get_computer_group_by_id(GROUP_ID)
+
+# If there are members of the Group, grab their device IDs and pass them into the redeploy_management_framework_v1 function
+if computer_group_data.computers:
+    computers = computer_group_data.computers or []
+
+    computer_ids = []
+
+    for computer in computers:
+        computer_ids.append(computer.id)
+
+    action = client.pro_api.redeploy_management_framework_v1(computer_ids)
+    print(action)
+else:
+    print("No Computers in Group")
