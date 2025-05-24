e15bb42612f5ce19ce2bb27dd7a34f07bb9e4d45
Jonathan Porter <j.porter712@gmail.com>
2025-05-24 13:51:46 -0700

    Update webhook_redeploy.py

--- scripts/webhook_redeploy.py
+++ scripts/webhook_redeploy.py
@@ -5,4 +5,4 @@ from utils.jamf_client import client
 computer_id = sys.argv[1]
 
 print(f"Redeploying framework for computer ID {computer_id}")
-client.redeploy_management_framework(computer_id)
+client.redeploy_management_framework_v1(computer_id)
