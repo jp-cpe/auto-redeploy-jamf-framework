46bc354e9d30ff3f139063ab46982fd5cc346f20
Jonathan Porter <j.porter712@gmail.com>
2025-06-03 18:43:52 -0700

    Update Slack output

--- scripts/main.py
+++ scripts/main.py
@@ -3,12 +3,14 @@ import os
 
 GROUP_ID = os.getenv("GROUP_ID")
 
+print(f"Getting computers in group with ID={GROUP_ID}")
 computers = get_computers_in_group(GROUP_ID)
 
 if computers:
     computer_ids = [c.id for c in computers]
+    print(f"Redeploying framework for computer ID {computer_ids}")
     result = redeploy_framework(computer_ids)
-    print(f"Redeploy triggered for IDs: {computer_ids}")
+    print(f"Redeploy triggered for ID: {computer_ids}")
     print(result)
 else:
     print(f"No computers found in group with ID={GROUP_ID}")
--- scripts/webhook_redeploy.py
+++ scripts/webhook_redeploy.py
@@ -5,6 +5,6 @@ computer_ids = sys.argv[1]
 
 print(f"Redeploying framework for computer ID {computer_ids}")
 result = redeploy_framework(computer_ids)
-print(f"Redeploy triggered for IDs: {computer_ids}")
+print(f"Redeploy triggered for ID: {computer_ids}")
 print(result)
 
